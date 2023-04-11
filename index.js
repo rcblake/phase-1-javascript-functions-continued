3c524bda53f5336bafb9d21b100186460e7667cb
Ren Blake <rcblake06@gmail.com>
2023-04-11 10:29:10 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun (saturdayAction = `roller-skate`) {
+    return (`This Saturday, I want to ${saturdayAction}!`)
+}
+
+function mondayWork (mondayAction = `go to the office`) {
+    return (`This Monday, I will ${mondayAction}.`)
+}
+
+
+function wrapAdjective (flairStyle = `*`,) {
+    return function encouragingPrompt (flairText = `special`) {
+        return (`You are ${flairStyle}${flairText}${flairStyle}!`)
+        
+}
+}
+encouragingPrompt = wrapAdjective("!!!")
